f4dfa34c97c111bb645a070905face0f699b963d
cami44444 <berrioscamila611@gmail.com>
2026-08-03 16:21:03 -0400

    si12

---
+++ ejercicio 12.py
@@ -0,0 +1,28 @@
+#Creacion de listas en python
+
+lista=["Shirley, Camila, Herbert, Jhonatan; jhamil, max"]
+
+print(lista)
+
+array=[1,2,3,4,5,6,7,8,9,10]
+
+print (array)
+
+frutas=["manzana", "frutilla", "sandia", "kiwi"]
+
+print(frutas[1])
+#Modificar elementos de una lista
+frutas [0]="sandia"
+#Imprimir listas
+print (frutas)
+
+#insertar elemento      apend es para agregar o insertar
+frutas.append ("banana")
+
+print (frutas)
+
+#eliminar elemento de la lista        remove 
+frutas.remove ("kiwi")
+
+print (frutas)
+
